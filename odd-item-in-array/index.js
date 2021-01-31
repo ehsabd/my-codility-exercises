e20f5547ef356c2bf547ea1d31be2f0a5984b467
ehsabd <ehsabd@outlook.com>
2021-01-31 18:02:57 +0330

    Find odd item initial

---
+++ odd-item-in-array/index.js
@@ -0,0 +1,20 @@
+function getOddItem(arr){
+    let oddItem = null;
+    let counts = {};
+    arr.forEach(item => {
+        if (!(item in counts)){
+            counts[item] = 1;
+        }else{
+            counts[item]++;
+        }
+    });
+    for (let num in counts){
+        var count = counts[num];
+        if (count % 2 ==1){ return num}
+    }
+}
+
+
+console.log(getOddItem([8,2,8,2,8,5,8])); // should return 5
+console.log(getOddItem([5,3,4,5,3,5,5])); // should return 4
+console.log(getOddItem([5,3,5,5,3,5,1]));  // should return 1
